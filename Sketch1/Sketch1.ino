9a03f34e3c1ee77b38014905169340c993489751
Jouke Davids <j.s.davids@st.hanze.nl>
2023-05-23 10:55:34 +0200

    upload project

---
+++ Sketch1/Sketch1.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Sketch1.ino
+ Created:	5/23/2023 10:54:28 AM
+ Author:	Jouke
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
